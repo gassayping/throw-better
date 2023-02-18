4850d176cdb05158d0f434c201597b447aada477
Gassayping <105948217+gassayping@users.noreply.github.com>
2023-02-18 05:51:00 +0000

    Born again Minecraft broke so uhhh...

---
+++ src/Throw Better BP/scripts/Main.js
@@ -0,0 +1,52 @@
+import { world, system, Vector, Location } from '@minecraft/server';
+import { throwables } from "throwables.js";
+
+let playersThrowing = new Map();
+
+world.events.itemStartCharge.subscribe(eventData => {
+	const item = eventData.itemStack.typeId;
+	if (!throwables[item]) return;
+	const player = eventData.source;
+	const throwLoop = system.runSchedule(() => {
+		if (!playersThrowing.has(player.id)) {
+			system.clearRunSchedule(throwLoop);
+			return;
+		}
+		fire(player, item, throwLoop);
+	}, throwables[item].fireRate)
+	playersThrowing.set(player.id, throwLoop);
+	fire(player, item, throwLoop);
+})
+
+world.events.itemStopCharge.subscribe(eventData => {
+	playersThrowing.delete(eventData.source.id);
+})
+
+function fire(player, item, scheduleId) {
+	if (!playersThrowing.has(player.id) || playersThrowing.get(player.id) !== scheduleId) {
+		system.clearRunSchedule(playersThrowing.get(player.id));
+		return;
+	}
+	const { x, y, z } = Vector.add(player.headLocation, player.viewDirection);
+	player.dimension.spawnEntity(throwables[item].projectile, new Location(x, y, z)).setVelocity(new Vector(player.viewDirection.x * throwables[item].projectileVelo, player.viewDirection.y * throwables[item].projectileVelo, player.viewDirection.z * throwables[item].projectileVelo));
+	const ammoObj = throwables[item].ammo;
+	if (!ammoObj) return;
+	ammoObj.consume ??= 1;
+	if (ammoObj.item) {
+		player.runCommandAsync(`clear @s ${item} 0 ${ammoObj.consume}`);
+	} else if (ammoObj.scoreboard) {
+		try {
+			ammoObj.scoreboard.id = world.scoreboard.addObjective(ammoObj.scoreboard.name, ammoObj.scoreboard.name);
+			ammoObj.scoreboard.id.setScore(player, ammoObj.scoreboard.max);
+		} catch {
+			ammoObj.scoreboard.id = world.scoreboard.getObjective(ammoObj.scoreboard.name);
+			ammoObj.scoreboard.id.setScore(player.scoreboard, ammoObj.scoreboard.max);
+		}
+		const ammo = player.scoreboard.getScore(ammoObj.scoreboard.id) - 1;
+		if (ammo <= 0) {
+			player.getComponent("minecraft:inventory").container.getSlot(player.currentSlot).clearItem();
+		} else {
+			player.scoreboard.setScore(ammoObj.scoreboard.id, ammo)
+		}
+	}
+}
---
+++ src/Throw Better BP/scripts/throwables.js
@@ -0,0 +1,29 @@
+export var throwables =
+{
+	"example:blow_gun": {
+		"fireRate": 20,
+		"projectile": "arrow",
+		"projectileVelo": 10,
+		"ammo": {
+			//"item": "arrow",
+			"consume": 1,
+			"scoreboard": {
+				"name": "blow_gun",
+				"max": 30
+			},
+			"emptyItem": "example:rock"
+		}
+	},
+	"example:rock": {
+		"fireRate": 1,
+		"projectile": "snowball",
+		"projectileVelo": 3,
+		"ammo": {
+			"scoreboard": {
+				"name": "rock",
+				"max": 30
+			},
+			"emptyItem": "example:empty"
+		}
+	}
+}
