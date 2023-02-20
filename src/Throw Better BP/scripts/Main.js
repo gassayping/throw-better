d34e3bb4edf4e1a76dbd998022d91d1088878bd3
Gassayping <105948217+gassayping@users.noreply.github.com>
2023-02-20 01:25:25 +0000

    Scoreboard ammo works now Backing up this version before things break

--- src/Throw Better BP/scripts/Main.js
+++ src/Throw Better BP/scripts/Main.js
@@ -1,4 +1,4 @@
-import { world, system, Vector, Location } from '@minecraft/server';
+import { world, system, Vector, Location, ItemStack, ItemTypes} from '@minecraft/server';
 import { throwables } from "throwables.js";
 
 for (const item in throwables) {
@@ -10,14 +10,15 @@ for (const item in throwables) {
 		world.scoreboard.addObjective(scoreboard.name, scoreboard.name);
 	} catch { }
 	scoreboard.id = world.scoreboard.getObjective(scoreboard.name);
-	console.warn(scoreboard.id);
 }
 
 let playersThrowing = new Map();
+let lastShot = new Map();
 
 world.events.itemStartCharge.subscribe(eventData => {
 	const item = eventData.itemStack.typeId;
 	if (!throwables[item]) return;
+	if (system.currentTick-lastShot.get(`${eventData.source.id}${item}`) < throwables[item].fireRate) return;
 	const player = eventData.source;
 	const throwLoop = system.runSchedule(() => {
 		if (!playersThrowing.has(player.id)) {
@@ -34,26 +35,36 @@ world.events.itemStopCharge.subscribe(eventData => {
 	playersThrowing.delete(eventData.source.id);
 })
 
-function fire(player, item, scheduleId) {
+async function fire(player, item, scheduleId) {
 	if (!playersThrowing.has(player.id) || playersThrowing.get(player.id) !== scheduleId) {
 		system.clearRunSchedule(playersThrowing.get(player.id));
 		return;
 	}
-	const { x, y, z } = Vector.add(player.headLocation, player.viewDirection);
-	player.dimension.spawnEntity(throwables[item].projectile, new Location(x, y, z)).setVelocity(new Vector(player.viewDirection.x * throwables[item].projectileVelo, player.viewDirection.y * throwables[item].projectileVelo, player.viewDirection.z * throwables[item].projectileVelo));
+	
+	let viewVector = player.viewDirection;
+	if(viewVector.y < -0.5){
+		viewVector.y *= 1.75;
+	}
+	const { x, y, z } = Vector.add(player.headLocation, viewVector);
+	const projectile = player.dimension.spawnEntity(throwables[item].projectile, new Location(x, y, z));
+	projectile.setVelocity(new Vector(viewVector.x * throwables[item].projectileVelo, viewVector.y * throwables[item].projectileVelo, viewVector.z * throwables[item].projectileVelo));
+	lastShot.set(`${player.id}${item}`, system.currentTick);
 	const ammoObj = throwables[item].ammo;
 	if (!ammoObj) return;
 	ammoObj.consume ??= 1;
 	if (ammoObj.item) {
 		player.runCommandAsync(`clear @s ${item} 0 ${ammoObj.consume}`);
 	} else if (ammoObj.scoreboard) {
-		ammoObj.scoreboard.id = world.scoreboard.getObjective(ammoObj.scoreboard.name);
-		ammoObj.scoreboard.id.setScore(player.scoreboard, ammoObj.scoreboard.max);
-		const ammo = player.scoreboard.getScore(ammoObj.scoreboard.id) - 1;
-		if (ammo <= 0) {
-			player.getComponent("minecraft:inventory").container.getSlot(player.currentSlot).clearItem();
-		} else {
-			ammoObj.scoreboard.id.setScore(player.scoreboard, ammo);
+		var ammo;
+		try {
+			ammo = player.scoreboard.getScore(ammoObj.scoreboard.id) - 1;
+		} catch {
+			await player.runCommandAsync(`scoreboard players add @s ${ammoObj.scoreboard.name} ${ammoObj.scoreboard.max}`);
+			ammo = player.scoreboard.getScore(ammoObj.scoreboard.id) - 1;
 		}
+		if (ammo <= 0) {
+				player.getComponent("minecraft:inventory").container.getSlot(player.selectedSlot).setItem(new ItemStack(ItemTypes.get(ammoObj.emptyItem)));
+		}
+			player.runCommandAsync(`scoreboard players set @s ${ammoObj.scoreboard.name} ${ammo}`);
 	}
 }
