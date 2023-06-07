83f7b5eaa73634c2db881281ba11c22203700128
Gassayping <Dev_Gassayping@proton.me>
2023-06-07 15:41:08 -0700

    Migrated to Typescript Updated script to be compatible with 1.20 Stopped throwable from throwing immediately after reloading

--- src/throw-better.ts
+++ src/throw-better.ts
@@ -1,38 +1,34 @@
-import { world, system, Vector, ItemStack } from '@minecraft/server';
+import { world, system, Vector, ItemStack, Player, Entity } from '@minecraft/server';
 import { reloadables } from './reloadables.js';
 import { throwables } from './throwables.js';
+
 for (const throwItem in throwables) {
-	if (!throwables.hasOwnProperty(throwItem))
-		continue;
+	if (!throwables.hasOwnProperty(throwItem)) continue;
 	const scoreboard = throwables[throwItem].ammo?.scoreboard;
-	if (!scoreboard)
-		continue;
+	if (!scoreboard) continue;
 	try {
 		world.scoreboard.addObjective(scoreboard.name, scoreboard.name);
-	}
-	catch { }
+	} catch { }
 	scoreboard.id = world.scoreboard.getObjective(scoreboard.name);
 }
 for (const reloadItem in reloadables) {
-	if (!reloadables.hasOwnProperty(reloadItem))
-		continue;
+	if (!reloadables.hasOwnProperty(reloadItem)) continue;
 	const scoreboard = reloadables[reloadItem].scoreboard;
 	const throwObj = throwables[reloadables[reloadItem].reloadedItem];
-	if (!scoreboard || !throwObj?.ammo.scoreboard)
-		continue;
+	if (!scoreboard || !throwObj?.ammo.scoreboard) continue;
 	scoreboard.id = throwObj.ammo.scoreboard.id;
 	scoreboard.max ??= throwObj.ammo.scoreboard.max;
 }
+
 const playersThrowing = new Map();
 const playersReloading = new Map();
 const lastShot = {};
+
 world.afterEvents.itemStartCharge.subscribe((eventData) => {
 	const item = eventData.itemStack.typeId;
-	if (!throwables[item] && !reloadables[item])
-		return;
-	if (system.currentTick - lastShot?.[`${eventData.source.id}${item}`] < throwables[item]?.fireRate)
-		return;
-	const player = eventData.source;
+	if (!throwables[item] && !reloadables[item]) return;
+	if (system.currentTick - lastShot?.[`${eventData.source.id}${item}`] < throwables[item]?.fireRate) return;
+	const player = eventData.source as Player;
 	if (reloadables[item]) {
 		startReload(player, item);
 		return;
@@ -51,6 +47,7 @@ world.afterEvents.itemStartCharge.subscribe((eventData) => {
 	playersThrowing.set(player.id, throwLoop);
 	fire(player, item, throwLoop);
 });
+
 world.afterEvents.itemStopCharge.subscribe(eventData => {
 	const playerId = eventData.source.id;
 	playersThrowing.delete(playerId);
@@ -59,7 +56,8 @@ world.afterEvents.itemStopCharge.subscribe(eventData => {
 		playersReloading.delete(playerId);
 	}
 });
-async function fire(player, item, scheduleId = 0) {
+
+async function fire(player: Player, item, scheduleId = 0) {
 	if (playersThrowing.get(player.id) !== scheduleId && scheduleId) {
 		system.clearRun(scheduleId);
 		return;
@@ -68,44 +66,39 @@ async function fire(player, item, scheduleId = 0) {
 	if (ammoObj) {
 		if (ammoObj.item) {
 			const hasItem = player.runCommand(`testfor @s[hasitem={item=${ammoObj.item}}]`);
-			if (hasItem.successCount !== 1)
-				return;
+			if (hasItem.successCount !== 1) return;
 			player.runCommand(`clear @s ${ammoObj.item} 0 ${ammoObj.consume ?? 1}`);
-		}
-		else if (ammoObj.scoreboard) {
-			let ammo;
+		} else if (ammoObj.scoreboard) {
+			let ammo: number;
 			try {
 				ammo = player.scoreboardIdentity.getScore(ammoObj.scoreboard.id);
-			}
-			catch {
+			} catch {
 				player.runCommand(`scoreboard players add @s ${ammoObj.scoreboard.name} ${ammoObj.scoreboard.max}`);
 				ammo = player.scoreboardIdentity.getScore(ammoObj.scoreboard.id);
 			}
 			if (ammo === 0) {
 				//@ts-ignore
 				player.getComponent('minecraft:inventory')?.container.getSlot(player.selectedSlot).setItem(new ItemStack(ammoObj.scoreboard.emptyItem));
-			}
-			else {
+			} else {
 				player.scoreboardIdentity.setScore(ammoObj.scoreboard.id, --ammo);
 			}
 		}
 	}
 	let viewVector = player.getViewDirection();
 	const playerRotation = player.getRotation();
-	if (playerRotation.x > 30)
-		viewVector = Vector.multiply(viewVector, 2.1);
+	if (playerRotation.x > 30) viewVector = Vector.multiply(viewVector, 2.1);
 	const spawnLoc = Vector.add(player.getHeadLocation(), viewVector);
-	const projectile = player.dimension.spawnEntity(throwables[item].projectile, spawnLoc);
+	const projectile = player.dimension.spawnEntity(throwables[item].projectile, spawnLoc) as Entity;
 	projectile.setRotation({ 'x': -playerRotation.x, 'y': -playerRotation.y });
 	projectile.applyImpulse(Vector.multiply(viewVector, throwables[item].projectileVelo));
 	lastShot[`${player.id}${item}`] = system.currentTick;
 }
-async function startReload(player, item) {
+
+async function startReload(player: Player, item: String) {
 	const reloadItem = reloadables[item];
 	const reloadAmmo = reloadItem.ammo;
 	const hasItem = player.runCommand(`testfor @s[hasitem={item=${reloadAmmo.item},quantity=${reloadAmmo.amount}..}]`);
-	if (hasItem.successCount !== 1)
-		return;
+	if (hasItem.successCount !== 1) return;
 	const reloadTimer = system.runTimeout(() => {
 		player.runCommand(`clear @s ${reloadAmmo.item} 0 ${reloadAmmo.amount}`);
 		//@ts-ignore
@@ -113,14 +106,13 @@ async function startReload(player, item) {
 		const slot = inv.getSlot(player.selectedSlot);
 		lastShot[`${player.id}${reloadItem.reloadedItem}`] = system.currentTick;
 		slot.setItem(new ItemStack(reloadItem.reloadedItem));
-		if (!reloadItem.scoreboard)
-			return;
+		if (!reloadItem.scoreboard) return;
 		try {
 			player.scoreboardIdentity.setScore(reloadItem.scoreboard.id, reloadItem.scoreboard.max);
-		}
-		catch {
+		} catch {
 			player.runCommand(`scoreboard players set @s ${reloadItem.scoreboard.name} ${reloadItem.scoreboard.max}`);
 		}
 	}, reloadItem.reloadTime);
 	playersReloading.set(player.id, reloadTimer);
 }
+
--- src/throwables.js
+++ src/throwables.js
@@ -1,4 +1,4 @@
-export var throwables =
+export const throwables =
 {
 
 }
